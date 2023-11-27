ade2ab8dd6239391050d1b85cbdeb9a9910fce67
Fonta97 <fontana.alessandro97@gmail.com>
2023-11-27 15:06:22 +0100

    feat: added randomNumber function and sumEvenOrOdd

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,3 +1,37 @@
 
-const nUser = prompt("Gerry è tornato. Ti punta il ferro e ti urla di dargli un numero!");
+const userChoice = prompt("Scegli se PARI o DISPARI");
+const nUser = prompt("Gerry è tornato. Ti punta il ferro e ti urla di dargli un numero da 1 a 5!");
+console.log(nUser);
+console.log(userChoice);
+
+const nComputer = randomNumber()
+console.log(nComputer);
+console.log(sumEvenOrOdd())
+
+if (userChoice === PARI) {
+    if (sumEvenOrOdd === "pari") {
+        document
+    }
+}
+
+
+
+//funzioni
+
+function sumEvenOrOdd() {
+    let nSum = nUser + randomNumber()
+    let pari = "pari"
+    let dispari = "dispari"
+
+    if (nSum % 2 === 0) {
+        return pari
+    } else {
+        return dispari
+    }
+}
+
+// funzione numero random
+function randomNumber() {
+    return Math.floor(Math.random() * 5 + 1)
+}
 
